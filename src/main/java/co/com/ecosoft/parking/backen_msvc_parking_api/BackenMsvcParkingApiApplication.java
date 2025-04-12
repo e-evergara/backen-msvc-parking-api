780be34ba80d9f5e57374ddebac92960594b4807
evergara <emersonjb@gmail.com>
2025-04-12 09:53:59 -0500

    Commit Init

---
+++ src/main/java/co/com/ecosoft/parking/backen_msvc_parking_api/BackenMsvcParkingApiApplication.java
@@ -0,0 +1,13 @@
+package co.com.ecosoft.parking.backen_msvc_parking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackenMsvcParkingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackenMsvcParkingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/ecosoft/parking/backen_msvc_parking_api/BackenMsvcParkingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.ecosoft.parking.backen_msvc_parking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackenMsvcParkingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
